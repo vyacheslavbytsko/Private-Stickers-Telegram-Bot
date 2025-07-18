bca581d45d081768988733e383e6ed62ce01e4e6
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-07-18 06:38:18 +0300

    ещё один фикс

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 cd /home/tecoxivebe38/Private-Stickers-Telegram-Bot
 docker build -t private-stickers-telegram-bot .
 exec docker run --rm \
+  -u 1000:1000 \
   --name privsticksbot \
   -v "$(pwd)/data:/app/data" \
   private-stickers-telegram-bot
